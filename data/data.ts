5dd6b61814e5651ab09fcafc27c11290b4d42eaf
kassraniroumand <148619288+kassraniroumand@users.noreply.github.com>
2024-07-05 18:40:22 +0100

    Update data.ts

--- data/data.ts
+++ data/data.ts
@@ -1,11 +1,11 @@
 const currentYear = new Date().getFullYear();
 const data = {
-    sitename: "Sandip Baikare Blog",
-    sitetagline: "Something great is on the way! ✨ 🔥",
+    sitename: "nullify project",
+    sitetagline: "nullify project",
     siteurl: "https://github.com/baikaresandip",
     sitelogo: "",
     title: "Coming Soon!",
-    description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
+    description: "nullify project",
     newsletterheading: "Stay tuned for our live updates!",
     copyrightText: `Copyright © ${currentYear} | Design and Developed By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://github.com/baikaresandip">Baikare Sandip</a>`,
     socialIconsHeading: "Follow Us 📣",
